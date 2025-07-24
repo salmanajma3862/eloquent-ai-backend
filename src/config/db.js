85d438e4e686395680d8c55f795f2a96743c384b
salmanajma3862 <salmanajmal412@gmail.com>
2025-07-24 12:48:25 +0500

    fix: update MongoDB connection logic and improve error handling

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,18 @@
+// src/config/db.js
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(`${process.env.MONGODB_URI}/eloquent-ai-db`);
-        console.log(`MongoDB Connected`);
-    } catch (error) {
-        console.error(`Error connecting to MongoDB: ${error.message}`);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+     dbName: 'eloquent-ai-db', 
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
