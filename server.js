9818fd6d89fbc3241e1e1a84db4298bce8829f74
ayanakhtar1 <www.ayanakhtarchachar786@gmail.com>
2025-08-02 13:17:31 -0700

    revert back to previous commit

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  maxAge: 86400 // 24 hours
+  maxAge: 86400 // 24 hours 
 }));
 
 app.use('/api/auth', authRoutes);
